daa1f3c5be27d2f6ad8fd67d0efc9944188df3e8
TemaSaur <TemaSaur@gmail.com>
2020-12-05 18:35:41 +0500

    system tray icon

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import sys
 import os
+from PyQt5.QtWidgets import QApplication, QSystemTrayIcon, QMenu
+from PyQt5.QtGui import QIcon
 
 
 # main function
 def main():
-	print("hello world")
+	app = QApplication(sys.argv)
+
+	tray_icon = QSystemTrayIcon(QIcon("src/icon.png"), parent=app)
+	tray_icon.setToolTip("Hello Icon")
+	tray_icon.show()
+	
+	sys.exit(app.exec_())
 
 
 if __name__ == "__main__":

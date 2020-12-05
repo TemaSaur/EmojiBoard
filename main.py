8db6cd3b9f19fab80cd3537fd9786b24cf1ecb8a
TemaSaur <TemaSaur@gmail.com>
2020-12-05 19:51:30 +0500

    working window, sys tray menu

--- main.py
+++ main.py
@@ -1,16 +1,41 @@
 import sys
 import os
-from PyQt5.QtWidgets import QApplication, QSystemTrayIcon, QMenu
+from PyQt5.QtWidgets import QApplication, QSystemTrayIcon, QMenu, QMainWindow
 from PyQt5.QtGui import QIcon
+
+
+X_POS = 200
+Y_POS = 200
+WIDTH = 400
+HEIGHT = 300
+
+NAME = "EmojiBoard;)"
+ICON_PLACE = "src/icon.png"
 
 
 # main function
 def main():
 	app = QApplication(sys.argv)
 
-	tray_icon = QSystemTrayIcon(QIcon("src/icon.png"), parent=app)
-	tray_icon.setToolTip("Hello Icon")
+	# set window
+	window = QMainWindow()
+	window.setGeometry(X_POS, Y_POS, WIDTH, HEIGHT)
+	window.setWindowTitle(NAME)
+	window.setWindowIcon(QIcon(ICON_PLACE))
+
+	# set tray icon
+	tray_icon = QSystemTrayIcon(QIcon(ICON_PLACE), parent=app)
+	tray_icon.setToolTip(NAME)
+
+	# set sys tray menu
+	menu = QMenu()
+	exit_action = menu.addAction("Exit")
+	exit_action.triggered.connect(app.quit)
+
+	tray_icon.setContextMenu(menu)
+
 	tray_icon.show()
+	window.show()
 	
 	sys.exit(app.exec_())
 

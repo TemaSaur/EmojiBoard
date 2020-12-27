6e242593269cc6ee31dff9f70ab1854962c4e80f
TemaSaur <TemaSaur@gmail.com>
2020-12-27 11:17:20 +0500

    working scrollarea

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import sys
 import os
-from PyQt5.QtWidgets import QApplication, QSystemTrayIcon, QMenu, QMainWindow,\
-	QPushButton, QScrollArea, QScrollBar
+from PyQt5.QtWidgets import QApplication, QSystemTrayIcon, QMenu, QMainWindow, \
+	QPushButton, QScrollArea, QWidget, QGridLayout, QWidget, QSizePolicy
 from PyQt5.QtGui import QIcon
 import emojis
 import pyperclip
-
 
 X_POS = 200
 Y_POS = 200
@@ -15,6 +14,7 @@ HEIGHT = 300
 NAME = "EmojiBoard;)"
 ICON_PLACE = "src/icon.png"
 EMOJIS = emojis.db.get_emoji_aliases()
+
 
 # TEST
 # emoji_ = emojis.encode(":stuck_out_tongue_winking_eye:")
@@ -32,10 +32,8 @@ def add_to_clipboard(txt):
 # 	return emoji_
 
 
-
 # main function
 def main():
-
 	app = QApplication(sys.argv)
 
 	# set window
@@ -55,10 +53,24 @@ def main():
 
 	# add scrollable part
 	scroll = QScrollArea(parent=window)
-	# scroll.setWidgetResizable(True)
-	scroll.setGeometry(10, 10, WIDTH-20, HEIGHT-20)
-	# scroll.setVerticalScrollBar(QScrollBar())
-	scroll.setVerticalScrollBarPolicy(True)
+	scroll.setGeometry(10, 10, WIDTH - 20, HEIGHT - 20)
+	scroll.setFixedWidth(WIDTH - 20)
+	scroll.setMinimumHeight(HEIGHT - 20)
+	scroll.setWidgetResizable(True)
+	# scroll.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
+	# doesn't work
+
+	scroll_contents = QWidget()
+	scroll_contents.setFixedWidth(WIDTH - 40)
+	scroll.setWidget(scroll_contents)
+
+	scroll_grid = QGridLayout()
+	scroll_grid.setHorizontalSpacing(4)
+	scroll_grid.setVerticalSpacing(10)
+	scroll_contents.setLayout(scroll_grid)
+
+	# scroll_grid.setColumnStretch(1, 4)
+	# scroll_grid.setColumnStretch(2, 4)
 
 	# the functionality
 	# TODO:
@@ -67,22 +79,32 @@ def main():
 	emoji_buttons = []
 
 	for a, b in EMOJIS.items():
-		butt = QPushButton(b, parent=scroll)
-		butt.setGeometry(10, i*35, 30, 30)
+		butt = QPushButton(b, parent=scroll_contents)
+		butt.setFixedWidth(36)
+		butt.setFixedHeight(36)
+
+		size_policy = QSizePolicy(QSizePolicy.Maximum,
+				QSizePolicy.Fixed)
+		size_policy.setHorizontalStretch(0)
+		size_policy.setVerticalStretch(0)
+		butt.setSizePolicy(size_policy)
+
+		# (10, i*35, 30, 30)
 		butt.setToolTip(a)
-		butt.show()
+		scroll_grid.addWidget(butt, i // 8, i % 8)
 		emoji_buttons.append(butt)
+		butt.show()
 		i += 1
 
 	for butt in emoji_buttons:
 		butt.clicked.connect(lambda: add_to_clipboard(butt.text()))
-		# butt.clicked.connect(lambda: add_to_clipboard(b))
+	# butt.clicked.connect(lambda: add_to_clipboard(b))
 
 	tray_icon.setContextMenu(menu)
 
 	tray_icon.show()
 	window.show()
-	
+
 	sys.exit(app.exec_())
 
 
